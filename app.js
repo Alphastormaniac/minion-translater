309f4210c097e4ab5ec5476b9ad48963dac3acf7
Sai Pavan <sai.pavan7829@gmail.com>
2021-09-24 21:00:16 +0530

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+var textarea = document.querySelector("textarea")
+var buttonPrimary = document.querySelector(".btn-primary")
+var input = document.querySelector("#inputButton")
+
+var inputButton = document.querySelector("input[name]")
+
+console.log(textarea)
+console.log(buttonPrimary)
+console.log(input)
+console.log(inputButton)
+
