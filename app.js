531dad1c88dc3c190d4966cf8a0a68829e45dc2c
Sai Pavan <sai.pavan7829@gmail.com>
2021-09-26 02:33:24 +0530

    typed in some cool js

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ var btnTranslator = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
-var outputToUser = document.querySelector("#output")
+var outputToUser = document.querySelector("#output");
 
-function clickHandler() {
-    outputToUser.innerHTML= txtInput.value;
+// var serverURL = "https://lesssonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
 }
 
-btnTranslator.addEventListener("click", clickHandler);
+function clickHandler() {
+    const inputText = txtInput.value; //
+    //    calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputToUser.innerText = translatedText;
+        })
+        .catch(errorEventHandler)
+}
 
+function errorEventHandler(error) {
+    alert("Something is wrong with the server.. Kindly co-operate.")
+}
 
+btnTranslator.addEventListener("click", clickHandler); //on click function is executed
